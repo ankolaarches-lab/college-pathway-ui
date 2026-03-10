36e0920e5942a27e90b08b78fbbaca8c7c9e4ad6
Arches Ankola <archesankola@Mac.attlocal.net>
2026-03-10 13:08:26 -0700

    feat: add Google Tag Manager (GTM-MM9DP8FL)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* Google Tag Manager */}
+      <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-MM9DP8FL');` }} />
+
+      {/* Google Analytics */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-072M5K6XF8"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-072M5K6XF8');
+          `,
+        }}
+      />
       <body className="min-h-screen bg-slate-50 antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MM9DP8FL" height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
+
         <AuthProvider>
           {/* Navigation Header */}
           <Header />
